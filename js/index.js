bd808328aa0bb952d306a35aac830bc148a9198a
RoggiDev <yael.rodriguez.g@outlook.es>
2026-01-30 00:04:26 -0600

    Add experience animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.set(".c-timeline-line", {
+    height: "0%",
+  });
+
+  gsap.set(".c-timeline-dot", {
+    top: "0%",
+  });
+
+  gsap.to(".c-timeline-line", {
+    height: "100%",
+    scrollTrigger: {
+      trigger: ".c-timeline",
+      start: "top center",
+      end: "bottom center",
+      scrub: true,
+      // markers: true,
+    },
+  });
+
+  gsap.to(".c-timeline-dot", {
+    top: "100%",
+    scrollTrigger: {
+      trigger: ".c-timeline",
+      start: "top center",
+      end: "bottom center",
+      scrub: true,
+      // markers: true,
+    },
+  });
+});
+
 document.querySelectorAll('.offcanvas a[href^="#"]').forEach((link) => {
   link.addEventListener("click", () => {
     const offcanvasEl = document.querySelector(".offcanvas");
