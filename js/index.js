0f1e986206b3adee8a06dd6ad4877ac1a7860560
RoggiDev <yael.rodriguez.g@outlook.es>
2026-02-02 22:10:44 -0600

    Add projects section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
+  const lenis = new Lenis();
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
   gsap.set(".c-timeline-line", {
     height: "0%",
   });
